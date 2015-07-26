793df26ac4841e7c8d79f12f7575f1f257f31aae
zweed4u <zackweeden@aol.com>
2015-07-25 20:04:35 -0400

    icon folder added for impending downloads-backup of txt

--- itemHashDestiny.py
+++ itemHashDestiny.py
@@ -13,7 +13,7 @@ for loc_val in loc_values:
 	if len((str(item).split('items/')))==1:
 		pass
 	else:
-		print (str(item).split('items/'))[1][:(str(item).split('items/'))[1].replace('-', ' ' ,1).index(' ')]+' - '+(str(item).split('items/'))[1][(str(item).split('items/'))[1].replace('-', ' ' ,1).index(' ')+1:].replace("-"," ").title()
+		#print (str(item).split('items/'))[1][:(str(item).split('items/'))[1].replace('-', ' ' ,1).index(' ')]+' - '+(str(item).split('items/'))[1][(str(item).split('items/'))[1].replace('-', ' ' ,1).index(' ')+1:].replace("-"," ").title()
 		f.write((str(item).split('items/'))[1][:(str(item).split('items/'))[1].replace('-', ' ' ,1).index(' ')]+' - '+(str(item).split('items/'))[1][(str(item).split('items/'))[1].replace('-', ' ' ,1).index(' ')+1:].replace("-"," ").title()+'\n')
 f.close()
 print '\nComplete! File saved to:\n'+os.getcwd()+'/itemHashCatalog.txt\n'
